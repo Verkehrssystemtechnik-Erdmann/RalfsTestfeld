ccad1b2227e1966909e42642413cd18987cfedcd
Verkehrssystemtechnik-Erdmann <raerdmann@th-wildau.de>
2020-02-14 10:56:48 +0100

    Projektdateien hinzufügen.

---
+++ RalfsTestfeld/RalfsTestfeldDefNSubfunc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "RalfsTestfeldDefNSubfunc.h" // Einbinden des Headers 
+
+int WertBerechnen(int Wert1, int Wert2)
+{
+	int Ausgabewert = Wert1 + Wert2;
+	return Ausgabewert;
+}
+
+double WertBerechnen(double Wert1, double Wert2) //  Überladen der Funktion: Funktion wird nach Struktur der Übergebenen Variablen ausgewählt. 
+{
+	double Ausgabewert = (Wert1 + Wert2) * 100;
+	return Ausgabewert;
+}
---
+++ RalfsTestfeld/RalfsTestfeldDefNSubfunc.h
@@ -0,0 +1,4 @@
+// Headerdatei für ausgelagerte Funktionen
+
+int WertBerechnen(int Wert1, int Wert2); // Hier wird nur deklariert, aber die Funktion nicht beschrieben
+double WertBerechnen(double Wert1, double Wert2); //  Überladen der Funktion
